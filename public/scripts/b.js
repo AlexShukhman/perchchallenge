c10814a8c8a59d0cdebff881de25975464035250
alexshukhman <alex.jacob.shukhman@gmail.com>
2019-01-04 00:25:13 -0500

    almost done, just have to make table

--- public/scripts/b.js
+++ public/scripts/b.js
@@ -1,9 +1,15 @@
+// Table Setup
+
+
 // Canvas Setup
 var canvas = document.getElementById('bCanvas');
 var img = document.getElementById('logo');
 var ctx = canvas.getContext('2d');
 ctx.fillStyle = '#F0F0F0';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.lineWidth = "1";
+ctx.fillStyle = 'gray';
+ctx.font = '10px Lato'
 
 // Socket Setup
 var socket = io();
@@ -17,4 +23,150 @@ socket.on('connect', (data) => {
 // Draw
 socket.on('update', (data) => {
     ctx.drawImage(img, data.x-20, data.y-28);
+    analyzePoint(data.x, data.y);
 });
+
+// Replay
+function replay() {
+    ctx.fillStyle = '#F0F0F0';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'gray';
+    getPoints(reDraw);
+    for (var i = 0; i < boxes.length; i++) {
+        drawBox(boxes[i], 'gray');
+        drawNumber(boxes[i][0][0], boxes[i][1][0], i)
+    }
+}
+
+// Draw 10x Faster
+function reDraw(o) {
+    drawx10(o.x, o.y);
+}
+
+function drawx10(xs, ys) {
+    if (xs.length != 0){
+        ctx.drawImage(img, xs.shift(), ys.shift());
+        setTimeout((()=>{drawx10(xs, ys)}), 10);
+    }
+}
+
+// Velocity Code
+var velocities = [];
+var allPoints = [];
+var boxes = [];
+
+getPoints(pointsSetup);
+
+for (var i = 0; i < 10; i++) {
+    velocities.push([]);
+    boxes.push(makeBox(i));
+}
+
+function makeBox(num) {
+    var max = 500;
+    var x = [];
+    var y = [];
+    for (var i = 0; i < 2; i++){
+        x.push(Math.floor(Math.random() * max));
+        y.push(Math.floor(Math.random() * max));
+    }
+    xn = [Math.min(...x), Math.max(...x)];
+    yn = [Math.min(...y), Math.max(...y)];
+    drawBox([xn,yn], 'gray');
+    drawNumber(xn[0], yn[0], num);
+    return [xn, yn];
+}
+
+function drawBox(box, color) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.rect(box[0][0], box[1][0], box[0][1]-box[0][0], box[1][1]-box[1][0]);
+    ctx.stroke();
+}
+
+function drawNumber(x, y, num) {
+    ctx.fillText(num.toString(), x+2, y+10);
+}
+
+function checkBox(i, x, y) {
+    var box = boxes[i];
+    return x<=box[0][1] && x>=box[0][0] && y<=box[1][1] && y>=box[1][0];
+}
+
+function getPoints(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.status == 200 && xhr.readyState == 4) {
+            callback(JSON.parse(xhr.response));
+        }
+    }
+    xhr.open('GET', '/getOut');
+    xhr.send();
+}
+
+function pointsSetup(o) {
+    for (var i = 0; i < o.x.length; i++){
+        analyzePoint(o.x[i], o.y[i]);
+    }
+}
+
+function analyzePoint(x, y) {
+    if (allPoints.length < 1){
+        allPoints.push([x,y]);
+        return;
+    }
+    else if (allPoints.length < 2){
+        var x0 = allPoints[allPoints.length-1][0]
+        var y0 = allPoints[allPoints.length-1][1]
+        for (var i = 0; i < boxes.length; i++) {
+            var b0 = checkBox(i, x0, y0);
+            var b1 = checkBox(i, x, y);
+            if (b0) {
+                if (b1) {
+                    drawBox(boxes[i], 'green');
+                }
+                else {
+                    velocities[i].push(velocity(x0, y0, x, y));
+                }
+            }
+            else if (b1) {
+                velocities[i].push(velocity(x0, y0, x, y));
+            }
+        }
+        allPoints.push([x,y]);
+        return;
+    }
+    var x0 = allPoints[allPoints.length-2][0]
+    var y0 = allPoints[allPoints.length-2][1]
+    var x1 = allPoints[allPoints.length-1][0]
+    var y1 = allPoints[allPoints.length-1][1]
+    for (var i = 0; i < boxes.length; i++) {
+        var b0 = checkBox(i, x0, y0);
+        var b1 = checkBox(i, x1, y1);
+        var b2 = checkBox(i, x, y);
+        if (b1) {
+            if (b2) {
+                drawBox(boxes[i], 'green');
+                if (b0) {
+                    velocities[i].push(velocity(x0, y0, x1, y1));
+                }
+            }
+            else {
+                drawBox(boxes[i], 'gray');
+                velocities[i].push(velocity(x1, y1, x, y));
+            }
+        }
+        else if (b2) {
+            drawBox(boxes[i], 'green');
+            velocities[i].push(velocity(x1, y1, x, y));
+        }
+        else {
+            drawBox(boxes[i], 'gray');
+        }
+    }
+    allPoints.push([x, y]);
+}
+
+function velocity(x0, y0, x1, y1) {
+    return 10 * Math.sqrt((x0-x1)**2+(y0-y1)**2);
+}
--- routes.js
+++ routes.js
@@ -1,4 +1,6 @@
 var router = require('express').Router();
+var fs = require('fs');
+var parse = require('csv-parse');
 
 router.get('/', (req, res) => {
     res.render('index');
@@ -10,6 +12,24 @@ router.get('/a', (req, res) => {
 
 router.get('/b', (req, res) => {
     res.render('b');
+});
+
+router.get('/getOut', (req, res) => {
+    var file = fs.readFileSync('public/output/out.csv');
+    parse(file, (err, csv) => { // results in obj in arr: [{}]
+        var o = {
+            x: [],
+            y: [],
+            t: []
+        };
+        for (var i = 0; i < csv.length; i++) {
+            var line = csv[i];
+            o.x.push((line[0].length != 0)? parseInt(line[0], 10): Number.NEGATIVE_INFINITY);
+            o.y.push((line[1].length != 0)? parseInt(line[1], 10): Number.NEGATIVE_INFINITY);
+            o.t.push(new Date(line[2]));
+        }
+        res.send(JSON.stringify(o));
+    });
 });
 
 router.get('*', (req, res) => {
